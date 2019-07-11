e7071a71810497298f086e935a269d0cbd13ace4
斯周 <m17017309072@163.com>
2019-07-11 15:23:34 +0800

    增加移动端适配方案

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,18 @@ module.exports = {
         }
       }
     }
+  },
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-plugin-px2rem')({
+            rootValue: 75,
+            exclude: /(node_module)/,
+            minPixelValue: 3
+          })
+        ]
+      }
+    }
   }
 }
